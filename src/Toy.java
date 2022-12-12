18d1ca66c99a39079ebed5b410f00a1eb10fa07e
AndreiSusha <Andrey.Susha@gmail.com>
2022-12-12 10:07:06 +0200

    methods
    Added two methods

--- src/Toy.java
+++ src/Toy.java
@@ -2,8 +2,39 @@
 public class Toy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
+		
 	}
 
+}
+
+
+class ToyW 
+{
+	
+	public String name;
+	public int weight;
+	
+	public ToyW() {
+		
+		this.name = " ";
+		this.weight = 0;
+		printData();
+		
+	}
+		
+	public ToyW (String name, int weight) {
+		this.name = name;
+		this.weight = weight;	
+		printData();
+		
+	}
+	
+	public printData() {
+		System.out.println("Brand: " + name);
+		System.out.println("Weright: " + weight);
+	}
+	
 }
